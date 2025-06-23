de40d277c8e48156eee42eebb04cd43c774e8ce4
davidjsm245 <120421579+davidjsm245@users.noreply.github.com>
2025-06-23 09:35:12 -0500

    ese es el diseño que mateus me paso

---
+++ src/kiosco_preview_mts/Kiosco_PREVIEW_MTS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kiosco_preview_mts;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Kiosco_PREVIEW_MTS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
